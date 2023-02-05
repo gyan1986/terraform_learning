dcf664b50d3188d83bb63d2e7196c4b90199fb04
gyanp <gyanp@DESKTOP-GNMPQ9K>
2023-02-05 15:40:54 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" { }
+
+variable "subnet_cidr_block" {
+  description = "subnet cidr block"  
+}
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {      
+        Name: "development" 
+        vpc_env: "dev" 
+    }
+  }
+  
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id = aws_vpc.development-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name: "dev-1-subnet"
+  }
+}
+
+output "dev-vpc" {
+  value = aws_vpc.development-vpc.id
+}
+
+output "dev-subnet" {
+  value = aws_subnet.dev-subnet-1.id
+}
