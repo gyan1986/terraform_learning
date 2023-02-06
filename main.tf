7f0faedf0f80484d46aca5b545f1a37551d5b1a8
gyanp <gyanp@DESKTOP-GNMPQ9K>
2023-02-06 14:25:08 +0530

    security_group_with_inound_outbound_rule

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -22,17 +23,6 @@ resource "aws_subnet" "dev-subnet-1" {
   }
 }
 
-/*resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-
-route {
-  cidr_block =  "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.myapp-igw.id
-}
-tags = {
-  Name: "${var.env_prefix}-rtb"
-}
-}*/
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
@@ -46,12 +36,43 @@ resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
   route {
-  cidr_block =  "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.myapp-igw.id
+   cidr_block =  "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.myapp-igw.id
+   }
+  tags = {
+   Name: "${var.env_prefix}-main-rtb"
+ }
+}
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
 
-}
-tags = {
-  Name: "${var.env_prefix}-main-rtb"
-}
+  ingress {
+   from_port = 22
+   to_port = 22
+   protocol = "tcp"
+   cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+   from_port = 8080
+   to_port = 8080
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks =["0.0.0.0/0"]
+   prefix_list_ids = []
+  }
+   
+  tags = {
+   Name: "${var.env_prefix}-sg"
+
+  }
+
 }
 
