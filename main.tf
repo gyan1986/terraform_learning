84a20200fa7b848217a1e0909836f0da18c8f442
gyanp <gyanp@DESKTOP-GNMPQ9K>
2023-02-06 12:49:02 +0530

    use_default_route_table

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "dev-subnet-1" {
   }
 }
 
-resource "aws_route_table" "myapp-route-table" {
+/*resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
 
 route {
@@ -32,7 +32,7 @@ route {
 tags = {
   Name: "${var.env_prefix}-rtb"
 }
-}
+}*/
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
@@ -42,7 +42,16 @@ resource "aws_internet_gateway" "myapp-igw" {
   
 }
 
-resource "aws_route_table_association" "a--rtb-subnet" {
-  subnet_id = aws_subnet.dev-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+  cidr_block =  "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.myapp-igw.id
+
 }
+tags = {
+  Name: "${var.env_prefix}-main-rtb"
+}
+}
+
